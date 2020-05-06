33903c47220e4f874fc977dab742e4f14f1c6f19
Peter <peter.lazarov94@gmail.com>
2020-05-06 12:24:48 +0300

    cors turned off

--- Startup.cs
+++ Startup.cs
@@ -58,11 +58,11 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(options => {
-                options.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-            });
+            // app.UseCors(options => {
+            //     options.AllowAnyOrigin()
+            //             .AllowAnyHeader()
+            //             .AllowAnyMethod();
+            // });
 
             app.UseAuthentication();
 
