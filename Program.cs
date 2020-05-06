3b9fa843397a578ec1e1cce83812fd1d1bfbc599
Peter <peter.lazarov94@gmail.com>
2020-05-06 11:10:58 +0300

    default port changed

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ namespace API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("https://*:8081", "http://*:8080");
                     webBuilder.UseStartup<Startup>();
                 });
     }
